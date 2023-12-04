1971a699b3b64f56d43e64f2cdc254ecaf98bf84
Mark Murphy <mark.murphy@n-able.com>
2023-12-04 12:12:42 +0000

    sets! another reduce!

--- day_4/part-two.js
+++ day_4/part-two.js
@@ -1,25 +1,21 @@
 const path = require('path');
 const fs = require('fs');
 
-const input = fs.readFileSync(path.join(__dirname, 'input.txt'), 'utf8').toString().trim().split('\n');
+const input = fs.readFileSync(path.join(__dirname, 'input.txt'), 'utf8').trim().split('\n');
 
-let amountOfCardsArray = Array(input.length).fill(1)
+let amountOfCardsArray = Array(input.length).fill(1);
 for (let i = 0; i < input.length; i++) {
     const line = input[i].replaceAll("  ", " ");
     const parts = line.split(':');
-    const winningNumbers = parts[1].split('|')[0].trim().split(' ');
-    const ourNumbers = parts[1].split('|')[1].trim().split(' ');
+    const winningNumbers = new Set(parts[1].split('|')[0].trim().split(' '));
+    const ourNumbers = new Set(parts[1].split('|')[1].trim().split(' '));
 
-    let numberOfMatches = 0;
-    ourNumbers.forEach(num => {
-        if (winningNumbers.includes(num)) {
-            numberOfMatches++;
-        }
-    });
+    const numberOfMatches = [...ourNumbers].reduce((count, num) => winningNumbers.has(num) ? count + 1 : count, 0);
 
     for (let j = 1; j <= numberOfMatches; j++) {
-        amountOfCardsArray[i+j] = amountOfCardsArray[i+j] + (1 * amountOfCardsArray[i])
+        const current = amountOfCardsArray[i+j];
+        amountOfCardsArray[i+j] = current + amountOfCardsArray[i];
     }
 }
 
-console.log(amountOfCardsArray.reduce((partialSum, a) => partialSum + a, 0))
+console.log(amountOfCardsArray.reduce((partialSum, a) => partialSum + a, 0));
